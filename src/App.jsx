9303df7af6a6a572746b8916f04b060f75fdd8dc
NunomValdez <nunomvmf@hotmail.com>
2023-12-27 19:11:54 +0000

    tabs dynamically working

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,51 @@
 import CoreConcepts from "./components/CoreConcepts";
 import Header from "./components/Header/Header";
-import { CORE_CONCEPTS } from "./data";
-import TabButton from './components/TabButton';
-import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
+import TabButton from "./components/TabButton";
+import { useState } from "react";
 
 function App() {
-  const handleClick = (selectedButton) =>{
-    console.log(selectedButton)
-    };
+  const [selectedTopic, setSelectedTopic] = useState("components");
+
+  const handleClick = (selectedButton) => {
+    setSelectedTopic(selectedButton);
+  };
+
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
         <section id="core-concepts">
-        <h2>Core Concepts</h2>
+          <h2>Core Concepts</h2>
           <ul>
-            <CoreConcepts {...CORE_CONCEPTS[0]}/>
-            <CoreConcepts {...CORE_CONCEPTS[1]}/>
-            <CoreConcepts {...CORE_CONCEPTS[2]}/>
+            <CoreConcepts {...CORE_CONCEPTS[0]} />
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
             {/* isto acima é o mesmo disto de baixo */}
-            <CoreConcepts title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/>
+            <CoreConcepts
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
+            />
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={()=>handleClick('components')} >Component</TabButton>
-            <TabButton onSelect={()=>handleClick('jsx')} >JSX</TabButton>
-            <TabButton onSelect={()=>handleClick('props')} >Props</TabButton>
-            <TabButton onSelect={()=>handleClick('state')} >State</TabButton>
+            <TabButton onSelect={() => handleClick("components")}>
+              Component
+            </TabButton>
+            <TabButton onSelect={() => handleClick("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleClick("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleClick("state")}>State</TabButton>
           </menu>
-
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
